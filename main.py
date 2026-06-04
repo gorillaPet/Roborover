86dbab286e9892c3e0484982d35a88393461f0df
Nick <gorillapet@proton.me>
2026-06-05 00:25:18 +1000

    bitwise operations to get raw 65000 - 0 numbers
    still work in progress

--- main.py
+++ main.py
@@ -3,50 +3,69 @@ import tkinter as tk
 from tkinter import ttk
 
 import sv_ttk
+from PIL import Image, ImageTk
 
 import pywinusb
+from pywinusb import hid
 
 
 window = tk.Tk()
-window.title ("Robo Rover")
-window.minsize (width=500, height=500)
+window.title("Robo Rover")
+window.minsize(width=500, height=500)
 
-sv_ttk.set_theme("dark")
+# Background image
+bg_image = Image.open("wheel.jpg")
+bg_photo = ImageTk.PhotoImage(bg_image)
 
-button = ttk.Button(text="Click me!")
-button.pack()
+bg_label = tk.Label(window, image=bg_photo)
+bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-from pywinusb import hid
 
 last = None
 
-def handler(data):
-    global last
+def reader(data):
+    
 
-    if last is None:
-        last = data
-        print(data)
-        return
+    # get left/right  
+    
+    a = data[1]
+    b = data[2]
+    
+    steering = a | b << 8
 
-    for i, (old, new) in enumerate(zip(last, data)):
-        if old != new:
-            print(f"Byte {i}: {old} -> {new}")
+    if steering > 32768:
+        left_percent = (32768 - steering) / 32768 * 100
+        print(left_percent)
+    if steering < 32768:
+        right_percent = (steering - 32768) / 32768 * 100
+        print(right_percent) 
+        
 
-    last = data.copy()
+    # print(result)
+
+    # get forward/backwards acceleration (128 - 32768 - 65408)
+    # a = data[9]
+    # b = data[10]
+    
+    # accel = a | b << 8
+
+    # result = accel
+
+    # print(result)
+
+    
+    
 
 devices = hid.HidDeviceFilter().get_devices()
 
 wheel = devices[28]
 wheel.open()
-wheel.set_raw_data_handler(handler)
+wheel.set_raw_data_handler(reader)
 
 input("Move wheel and pedals...")
 
-def handler(data):
-    steering = data[1] | (data[2] << 8)
-    print(steering)
-wheel.close()
 
+# def convert(data)
 
 # player = vlc.MediaPlayer('test.mp4')
 # player.play()
